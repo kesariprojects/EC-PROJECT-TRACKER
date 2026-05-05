b0977e8360ce9c424abe13fb7c678bf622c7a2a0
kesariprojects <prathamesh.kesariprojects@gmail.com>
2026-05-05 15:47:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import ReactDOM from 'https://esm.sh/react-dom@19.2.0/client?deps=react@19.2.0';
 import { html } from './jsx.js';
 import { Providers } from './mainProviders.js';
-import { App } from './App.js';
+import App from './App.js'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(html`<${Providers}><${App} /></${Providers}>`);
