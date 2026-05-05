24c1b7e2251cb38b57a87952205cdea384c48290
kesariprojects <prathamesh.kesariprojects@gmail.com>
2026-05-05 16:57:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
-    sourcemap: false
-  }
+    minify: 'esbuild',
+  },
 });
